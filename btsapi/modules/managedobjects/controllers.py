21449a39fffc3ece1d2faa25c1dbdf05a4366c21
Bodastage <bodastage@gmail.com>
2018-01-04 13:58:43 +0300

    Build MO tree

--- btsapi/modules/managedobjects/controllers.py
+++ btsapi/modules/managedobjects/controllers.py
@@ -33,8 +33,47 @@ def get_aci_tree_data(parent_pk):
 
     if vendor_pk is not None and tech_pk is not None and swversion_pk is None and parent_pk is not None \
             and search_term is not None and len(search_term) > 0:
-        query = ManagedObject.query.filter_by(vendor_pk=vendor_pk, tech_pk=tech_pk,parent_pk=parent_pk).\
-            filter(ManagedObject.name.ilike('%ed%'))
+        query = ManagedObject.query.filter_by(vendor_pk=vendor_pk, tech_pk=tech_pk).\
+            filter(ManagedObject.name.ilike('%{}%'.format(search_term))).filter(ManagedObject.pk != parent_pk)
+
+        # app.logger.info(str(query))
+        # app.logger.info("vendor_pk:{}, tech_pk:{} parent_pk:{}, search_term:{}".format(vendor_pk, tech_pk, parent_pk, search_term))
+        mo_children = []
+
+        aci_tree_nodes = []
+        for mo in query.all():
+            p_pk = mo.parent_pk
+            c_pk = mo.pk
+            c_name = mo.name
+
+            # app.logger.info("p_pk:{1}, c_pk:{0}, c_name: {2}".format(c_pk, p_pk, c_name))
+
+            # Mark whether an MO is before the parent
+            is_before_parent = False
+
+            while p_pk != parent_pk:
+                child_mo = ManagedObject.query.filter_by(pk=p_pk).first()
+
+                if child_mo is None:
+                    is_before_parent = True
+                    break
+                # app.logger.info("child_mo: {} pk:{}".format(child_mo.name, child_mo.pk))
+
+                p_pk = child_mo.parent_pk
+                c_pk = child_mo.pk
+                c_name = child_mo.name
+
+            if is_before_parent is True: continue
+
+            if c_name not in mo_children:
+                mo_aci_entries.append(dict(id=c_pk, label=c_name, inode=True, open=True))
+                mo_children.append(c_name)
+
+        # app.logger.info("Endi....")
+        return jsonify(mo_aci_entries)
+
+
+
 
     if query is None:
         return jsonify([])
@@ -75,8 +114,6 @@ def get_fields_in_mo_table(mo_pk):
 def get_dt_data(mo_pk):
     """Get managed objects values in jQuery datatables format"""
 
-    fields = []
-
     # Get the vendor and technology pk's
     managedobject = ManagedObject.query.filter_by(pk=mo_pk).first()
     vendor_pk = managedobject.vendor_pk
@@ -86,12 +123,9 @@ def get_dt_data(mo_pk):
     # Get the schema
     managedobject_schema = ManagedObjectSchema.query.filter_by(vendor_pk=vendor_pk, tech_pk=tech_pk).first()
     schema_name = managedobject_schema.name.lower()
-    mo_table_name = "{0}.{1}".format(schema_name, mo_name)
 
     metadata = MetaData()
     mo_data_table = Table(mo_name.lower(), metadata, autoload=True, autoload_with=db.engine, schema=schema_name)
-
-    fields = [c.name for c in mo_data_table.columns]
 
     columns = []
     for c in mo_data_table.columns:
