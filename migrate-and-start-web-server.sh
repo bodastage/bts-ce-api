49931ec772788e108ee378f877c8ddadf770a75b
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-06-11 05:59:25 +0300

    Return HTTP 204 code while database migrations are running

--- migrate-and-start-web-server.sh
+++ migrate-and-start-web-server.sh
@@ -2,6 +2,9 @@
 
 set -x 
 
+export FLASK_APP=wait_for_database.py
+flask run --host=0.0.0.0 --port=8181
+
 # Wait abit for the database to startup
 sleep 10
 
@@ -10,7 +13,11 @@ cd /migrations
 #Run migrations
 alembic upgrade head 
 
+# Stop the wait for database service 
+pid=$(ps -ef | grep "flask run " | grep -v grep | awk '{print $2}')
+kill -9 $pid 
+unset FLASK_APP
 
 cd /app
-#Start webserver
+#Start api service
 /usr/bin/gunicorn --config /app/gunicorn_config.py wsgi:app
---
+++ wait_for_database.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
+
+@app.route('/', defaults={'path': ''}, methods=['GET','POST'])
+@app.route('/<path:path>', methods=['GET','POST'])
+def wait_for_database_to_start(path):
+    return 'Database is still starting...', 204
+
+
+# if __name__ == '__main__':
+#     app.run()
